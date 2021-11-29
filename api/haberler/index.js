a933cff8cf1c24d321bc9e7b945f5d4725b0546f
Aziz2525 <aziz.app.developer@gmail.com>
2021-11-29 14:11:34 +0300

    29.11.2021

--- api/haberler/index.js
+++ api/haberler/index.js
@@ -8,4 +8,32 @@ export const cnn = () =>{
         return err
     })
 }
+export const haberlercom = () =>{
+    return axios.get(config.serviceUri+"/get/haberler/haberlercom/tum").then((res)=>{
+        return res.data.rss.channel[0].item
+    }).catch((err)=>{
+        return err
+    })
+}
+export const haberturk = () =>{
+    return axios.get(config.serviceUri+"/get/haberler/haberturk/tum").then((res)=>{
+        return res.data.rss.channel[0].item
+    }).catch((err)=>{
+        return err
+    })
+}
+export const ntv = () =>{
+    return axios.get(config.serviceUri+"/get/haberler/ntv/tum").then((res)=>{
+        return res.data.feed.entry
+    }).catch((err)=>{
+        return err
+    })
+}
+export const aa = () =>{
+    return axios.get(config.serviceUri+"/get/haberler/aa/tum").then((res)=>{
+        return res.data.rss.channel[0].item
+    }).catch((err)=>{
+        return err
+    })
+}
 
--- screens/Home.js
+++ screens/Home.js
@@ -10,7 +10,7 @@ import {
   RefreshControl
 } from "react-native";
 import theme from "../theme";
-import { cnn } from "../api/haberler";
+import { cnn, haberlercom, haberturk, ntv,aa } from "../api/haberler";
 import RenderHtml from "react-native-render-html";
 import ContentLoader, { Rect,Circle } from "react-content-loader/native";
 const { width, height } = Dimensions.get("window");
@@ -18,11 +18,20 @@ const Home = ({navigation}) => {
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = React.useState(false);
   const [cnnB, setCnnB] = useState([]);
+  const [haberlercomB, setHaberlercomB] = useState([]);
+  const [haberturkB, setHaberturkB] = useState([]);
+  const [ntvB, setNtvB] = useState([]);
+  const [aaB, setAaB] = useState([]);
+  const [activeTab,setActiveTab] = useState('CNN')
   useEffect(() => {
-    cnn().then((res) => {
-      setCnnB(res);
+    Promise.all([cnn(),haberlercom(),haberturk(),ntv(),aa()]).then((res)=>{
       setLoading(false);
-    });
+      setCnnB(res[0]);
+      setHaberlercomB(res[1])
+      setHaberturkB(res[2])
+      setNtvB(res[3])
+      setAaB(res[4])
+    })
   }, []);
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -86,6 +95,96 @@ const Home = ({navigation}) => {
   </>
     );
   };
+  const CNNHaber = () =>{
+     return cnnB.map((data, index) => {
+        const source = {
+          html: data.title[0],
+        };
+        return (
+          <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
+            <View style={styles.imageCard}>
+              <Image
+                source={{ uri: data.image[0] }}
+                style={[styles.cardImage]}
+              />
+            </View>
+            <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
+          </TouchableOpacity>
+        );
+      })
+  }
+  const HaberlerComHaber = () =>{
+    return haberlercomB.map((data, index) => {
+       const source = {
+         html: data.title[0],
+       };
+       return (
+         <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
+           <View style={styles.imageCard}>
+             <Image
+               source={{ uri: data["media:content"][0].$.url }}
+               style={[styles.cardImage]}
+             />
+           </View>
+           <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
+         </TouchableOpacity>
+       );
+     })
+ }
+ const HaberturkHaber = () =>{
+  return haberturkB.map((data, index) => {
+     const source = {
+       html: data.title[0],
+     };
+     return (
+       <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
+         <View style={styles.imageCard}>
+           <Image
+             source={{ uri: data["media:content"][0].$.url }}
+             style={[styles.cardImage]}
+           />
+         </View>
+         <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
+       </TouchableOpacity>
+     );
+   })
+}
+const NTVHaber = () =>{
+  return ntvB.map((data, index) => {
+     const source = {
+       html: data.title[0]._
+     };
+     return (
+       <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
+         <View style={styles.imageCard}>
+           <Image
+             source={{ uri: "https://i.ytimg.com/vi/XEJM4Hcgd3M/maxresdefault.jpg" }}
+             style={[styles.cardImage]}
+           />
+         </View>
+         <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
+       </TouchableOpacity>
+     );
+   })
+}
+const AAHaber = () =>{
+  return aaB.map((data, index) => {
+     const source = {
+       html: data.title[0],
+     };
+     return (
+       <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
+         <View style={styles.imageCard}>
+           <Image
+             source={{ uri: data.image[0] }}
+             style={[styles.cardImage]}
+           />
+         </View>
+         <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
+       </TouchableOpacity>
+     );
+   })
+}
   return (
     <ScrollView
       style={styles.container}
@@ -97,23 +196,26 @@ const Home = ({navigation}) => {
         />
       }
     >
+      <ScrollView horizontal style={{marginBottom:20}} showsHorizontalScrollIndicator={false}>
+        <TouchableOpacity style={activeTab === 'CNN'?styles.viewActiveItem:styles.viewItem} onPress={()=>setActiveTab('CNN')}>
+          <Text style={{color:activeTab === 'CNN'?'white':'black',fontWeight:activeTab === 'CNN'?'700':'400'}}>CNN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity  style={activeTab === 'HABERLERCOM'?styles.viewActiveItem:styles.viewItem} onPress={()=>setActiveTab('HABERLERCOM')}>
+          <Text style={{color:activeTab === 'HABERLERCOM'?'white':'black',fontWeight:activeTab === 'HABERLERCOM'?'700':'400'}}>Haberler.com</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.viewItem} style={activeTab === 'HABERTURK'?styles.viewActiveItem:styles.viewItem} onPress={()=>setActiveTab('HABERTURK')}>
+          <Text style={{color:activeTab === 'HABERTURK'?'white':'black',fontWeight:activeTab === 'HABERTURK'?'700':'400'}}>Habertürk</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.viewItem} style={activeTab === 'NTV'?styles.viewActiveItem:styles.viewItem} onPress={()=>setActiveTab('NTV')}>
+          <Text style={{color:activeTab === 'NTV'?'white':'black',fontWeight:activeTab === 'NTV'?'700':'400'}}>NTV</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.viewItem} style={activeTab === 'AA'?styles.viewActiveItem:styles.viewItem} onPress={()=>setActiveTab('AA')}>
+          <Text style={{color:activeTab === 'AA'?'white':'black',fontWeight:activeTab === 'AA'?'700':'400'}}>AA</Text>
+        </TouchableOpacity>
+      </ScrollView>
       {!loading
-        ? cnnB.map((data, index) => {
-            const source = {
-              html: data.title[0],
-            };
-            return (
-              <TouchableOpacity style={styles.card} key={index} onPress={()=>navigation.push('HaberDetay',{data:data})}>
-                <View style={styles.imageCard}>
-                  <Image
-                    source={{ uri: data.image[0] }}
-                    style={[styles.cardImage]}
-                  />
-                </View>
-                <RenderHtml contentWidth={width} source={source} baseStyle={{fontSize:18}}/>
-              </TouchableOpacity>
-            );
-          })
+        ? 
+          activeTab === "CNN"?CNNHaber():activeTab === "HABERLERCOM" ? HaberlerComHaber(): activeTab === "HABERTURK" ?HaberturkHaber(): activeTab === 'NTV' ? NTVHaber(): activeTab === 'AA' ?AAHaber():null
         : <>{Loading()}</>}
     </ScrollView>
   );
@@ -145,4 +247,24 @@ const styles = StyleSheet.create({
     width,
     marginBottom: 10,
   },
+  viewItem:{
+    height:25,
+    paddingLeft:10,
+    paddingRight:10,
+    backgroundColor:theme.style.bgGray,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:10,
+    marginRight:20
+  },
+  viewActiveItem:{
+    height:25,
+    paddingLeft:10,
+    paddingRight:10,
+    backgroundColor:theme.style.tabNavActiveColor,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:5,
+    marginRight:20
+  }
 });
